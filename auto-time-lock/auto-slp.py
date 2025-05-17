a18a312458f869c8f0de0b37715b48b42c147269
dig <70334760+diganta121@users.noreply.github.com>
2025-05-17 22:29:42 +0530

    Update auto-slp.py

--- auto-time-lock/auto-slp.py
+++ auto-time-lock/auto-slp.py
@@ -1,18 +1,15 @@
 import os
 import time
-import datetime
-
-prev = datetime.datetime.now()
 
 try:
     n = int(eval(input("expressions for number of seconds (-ve to -s): \n>>: ")))
     time.sleep(abs(n))
 
     if n < 0:
-        # shdn the system
+        print("shdwn the system")
         os.system("shutdown /s /t 1")
     else:
-        # to sleep
+        print("sleepy time")
         os.system("rundll32.exe powrprof.dll,SetSuspendState 0,1,0")
 except:
     print("Invalid input. Please enter a number.")
